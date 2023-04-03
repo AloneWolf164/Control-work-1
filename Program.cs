e3a60b3af8c6f317a12a8f051f391e40ff5073d2
Sergey Biryukov <sergeybiryukov21@gmail.com>
2023-04-03 22:48:18 +0400

    The task is solved using the method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using static System.Console;
 Write("Введите элементы массива через пробел: ");
 string[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 Write("Массив содержащий не более 3-х символов состоит из следующих элементов: ");
+ArrayWithThreeSimbols(array);
 
+void ArrayWithThreeSimbols(string[] array)
+{
 Write("[");
 for (int i = 0; i < array.Length; i++)
 {
@@ -14,37 +17,4 @@ for (int i = 0; i < array.Length; i++)
     }
 }
 Write("]");
-
-// Write("[");
-// if (array[0].Length <= 3) {Write(array[0]);}
-// if (array[1].Length <= 3) {Write(array[1]); Write(",");}
-// if (array[2].Length <= 3) {Write(array[2]); Write(",");}
-// if (array[3].Length <= 3) {Write(array[3]); Write(",");}
-// if (array[4].Length <= 3) {Write(array[4]); Write("]");}
-
-string[] NewArray(string[] array)
-{
-    string[] result = new string[array.Length];
-    int i = 0;
-    while (i < array.Length)
-    {
-        if (array[i].Length <= 3)
-        {
-            result[i] = array[i];
-            i++;
-        }
-        else
-        {
-            i++;
-        }
-    }
-    return result;
-}
-
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Write($"{array[i]} , ");
-    }
 }
