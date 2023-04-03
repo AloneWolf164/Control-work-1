d4077c3320661b38dac83a55a8a58de774890d57
Sergey Biryukov <sergeybiryukov21@gmail.com>
2023-04-03 20:17:57 +0400

    Create program with a primitive solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+// Write("Введите первый элемент исходного массива: ");
+// string[] array1 = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// Write("Введите второй элемент исходного массива: ");
+// string[] array2 = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// Write("Введите третий элемент исходного массива: ");
+// string[] array3 = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+string[] inArray = {"Denmark","Hel","2"};
+// string[] array1 = inArray[0].Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// string[] array2 = inArray[1].Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// string[] array3 = inArray[2].Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+if (inArray[0].Length <= 3) {WriteLine(inArray[0]);}
+if (inArray[1].Length <= 3) {WriteLine(inArray[1]);}
+if (inArray[2].Length <= 3) {WriteLine(inArray[2]);}
